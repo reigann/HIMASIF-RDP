5b56ebd96a46ba30a32cb477bcf65f25ddeecd39
nxtah <rizkynath543@gmail.com>
2025-04-14 21:13:04 +0700

    menambahkan page tentang himasif

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,11 @@ export default defineConfig({
                 'resources/css/components/navbar.css',
                 'resources/css/components/footer.css',
                 'resources/css/loading.css',
+                'resources/css/tentang-himasif.css',
                 'resources/js/app.js',
                 'resources/js/home.js',
-                'resources/js/loading.js'
+                'resources/js/loading.js',
+                'resources/js/tentang-himasif.js'
             ],
             refresh: true,
         }),
